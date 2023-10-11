2a8240f56819a5e4302362605338e4d705c01d4e
Jan Christoph Ebersbach <jceb@e-jc.de>
2023-10-11 16:44:31 +0200

    feat: support additional HTTP methods and catch error for non-JSON responses

--- lib.js
+++ lib.js
@@ -10,7 +10,7 @@ export const HTTPMethod = $.NullaryType("HTTPMethod")(
   "https://github.com/identinet/identinet#HTTPMethod",
 )([])((x) =>
   typeof x === "string" &&
-  ["get", "post", "push", "put"].includes(x.toLowerCase)
+  ["get", "post", "push", "put", "delete"].includes(x.toLowerCase)
 );
 
 export const URLType = $.NullaryType("URL")(
@@ -49,70 +49,64 @@ sanctuary.map((t) => (S.types[t.name] = t))(env);
 import * as Fluture from "https://cdn.skypack.dev/fluture";
 export const F = Fluture;
 
-export const log = (msg) =>
-  (value) => {
-    console.log(msg, S.show(value));
-    return value;
-  };
+export const log = (msg) => (value) => {
+  console.log(msg, S.show(value));
+  return value;
+};
 
 // buildURL :: String -> List(String) -> Maybe(Pair(HTTP_Method_String)(URL_String))
-export const buildURL = (baseURL) =>
-  (args) => {
-    if (S.not(S.is($.String)(baseURL)) || S.trim(baseURL).length === 0) {
-      return S.Nothing;
-    }
-    const joinWithBaseURL = (url) =>
-      S.joinWith("/")(S.map(S.trim)([baseURL, url]));
-    const joinQueryArgs = (queryArgs) =>
-      "?" + S.joinWith("&")(S.map(S.trim)(queryArgs));
-    const tailNothingIfEmpty = S.pipe([
-      S.tail,
-      S.ifElse(S.equals(S.Just(S.empty(Array))))(() => S.Nothing)(S.I),
-    ]);
+export const buildURL = (baseURL) => (args) => {
+  if (S.not(S.is($.String)(baseURL)) || S.trim(baseURL).length === 0) {
+    return S.Nothing;
+  }
+  const joinWithBaseURL = (url) =>
+    S.joinWith("/")(S.map(S.trim)([baseURL, url]));
+  const joinQueryArgs = (queryArgs) =>
+    "?" + S.joinWith("&")(S.map(S.trim)(queryArgs));
+  const tailNothingIfEmpty = S.pipe([
+    S.tail,
+    S.ifElse(S.equals(S.Just(S.empty(Array))))(() => S.Nothing)(S.I),
+  ]);
 
-    return S.pipe([
-      (list) => S.Pair(S.head(list))(S.tail(list)),
-      S.map(S.chain(S.pipe([
-        (urlSegments) =>
-          S.Pair(S.head(urlSegments))(tailNothingIfEmpty(urlSegments)),
-        S.map(S.map(joinQueryArgs)),
-        S.mapLeft(S.map(joinWithBaseURL)),
-        S.pair(S.concat),
-        // S.map((url) => new URL(url)),
-      ]))),
-      S.pair((fst) =>
-        (snd) =>
-          S.ifElse(
-            S.any(S.isNothing),
-          )(() => S.Nothing)(([f, s]) =>
-            S.Just(S.Pair(S.fromMaybe("")(f))(S.fromMaybe("")(s)))
-          )([fst, snd])
-      ),
-    ])(args);
-  };
+  return S.pipe([
+    (list) => S.Pair(S.head(list))(S.tail(list)),
+    S.map(S.chain(S.pipe([
+      (urlSegments) =>
+        S.Pair(S.head(urlSegments))(tailNothingIfEmpty(urlSegments)),
+      S.map(S.map(joinQueryArgs)),
+      S.mapLeft(S.map(joinWithBaseURL)),
+      S.pair(S.concat),
+      // S.map((url) => new URL(url)),
+    ]))),
+    S.pair((fst) => (snd) =>
+      S.ifElse(
+        S.any(S.isNothing),
+      )(() => S.Nothing)(([f, s]) =>
+        S.Just(S.Pair(S.fromMaybe("")(f))(S.fromMaybe("")(s)))
+      )([fst, snd])
+    ),
+  ])(args);
+};
 
 // fetchURL :: StrMap -> Pair(HTTP_Method_String)(URL_String) -> Future(Response)
-export const fetchURL = (initAttrs) =>
-  (urlPair) => {
-    const _fetch = (init) =>
-      (method) =>
-        (resource) => F.attemptP(() => fetch(resource, { ...init, method }));
+export const fetchURL = (initAttrs) => (urlPair) => {
+  const _fetch = (init) => (method) => (resource) =>
+    F.attemptP(() => fetch(resource, { ...init, method }));
 
-    return S.pair(_fetch(initAttrs))(urlPair);
-  };
+  return S.pair(_fetch(initAttrs))(urlPair);
+};
 
-// whenString :: (a -> a) -> a -> a
-const whenString = S.when(S.is($.String));
+// // whenString :: (a -> a) -> a -> a
+// const whenString = S.when(S.is($.String));
 
 // whenGetMethod :: (a -> a) -> a -> a
 const whenGetMethod = S.when((p) => S.equals("get")(S.toLower(S.fst(p))));
 
 // setDefaultSearchParam :: (String, String) -> URL -> URL
-const setSearchParam = (key, value) =>
-  (url) => {
-    url.searchParams.set(key, value);
-    return url;
-  };
+const setSearchParam = (key, value) => (url) => {
+  url.searchParams.set(key, value);
+  return url;
+};
 
 // setDefaultSearchParam :: (String, String) -> URL -> URL
 const setDefaultSearchParam = (key, value) =>
@@ -129,54 +123,53 @@ const toURL = (url) => {
 };
 
 // toJSON :: Response -> Object
-const toJSON = F.encaseP((r) => r.json());
+const toJSON = F.encaseP((response) =>
+  response.json().catch(() => response.body)
+);
 
 // listOfRemainingPageIndexes :: Response -> Array(Integer)
-const listOfRemainingPageIndexes = (r) =>
+const listOfRemainingPageIndexes = (response) =>
   S.range(2)(
     S.fromMaybe(1)(
-      S.parseInt(10)(r.headers.get("x-total-pages")),
+      S.parseInt(10)(response.headers.get("x-total-pages")),
     ),
   );
 
 // drainURL :: StrMap -> Pair(HTTP_Method_String)(URL_String) -> Future(Response)
-const drainURL = (initAttrs) =>
-  (urlPair) =>
-    S.pipe([
-      fetchURL(initAttrs),
-      S.chain(
-        (response) =>
-          S.pipe([
-            listOfRemainingPageIndexes,
-            S.map((pageidx) =>
-              S.map((url) =>
-                setSearchParam("page", new String(pageidx))(new URL(url))
-              )(urlPair)
-            ),
-            S.map(fetchURL(initAttrs)),
-            S.concat([F.resolve(response)]),
-            F.parallel(5),
-          ])(response),
-      ),
-      S.chain(S.pipe([
-        S.map(toJSON),
-        F.parallel(5),
-      ])),
-      S.map(S.join),
-    ])(urlPair);
+const drainURL = (initAttrs) => (urlPair) =>
+  S.pipe([
+    fetchURL(initAttrs),
+    S.chain(
+      (response) =>
+        S.pipe([
+          listOfRemainingPageIndexes,
+          S.map((pageidx) =>
+            S.map((url) =>
+              setSearchParam("page", new String(pageidx))(new URL(url))
+            )(urlPair)
+          ),
+          S.map(fetchURL(initAttrs)),
+          S.concat([F.resolve(response)]),
+          F.parallel(5),
+        ])(response),
+    ),
+    S.chain(S.pipe([
+      S.map(toJSON),
+      F.parallel(5),
+    ])),
+    S.map(S.join),
+  ])(urlPair);
 
 // gitLabFetch :: token -> Pair(HTTP_Method_String)(URL_String) -> Future(Response)
-export const gitLabFetch = (fetch_one) =>
-  (token) =>
-    (urlPair) => {
-      const initAttrs = { headers: { "PRIVATE-TOKEN": token } };
+export const gitLabFetch = (fetch_one) => (token) => (urlPair) => {
+  const initAttrs = { headers: { "PRIVATE-TOKEN": token } };
 
-      return S.pipe([
-        S.map(toURL),
-        whenGetMethod(S.map(setDefaultSearchParam("per_page", "100"))),
-        S.ifElse((urlPair) =>
-          fetch_one === true ||
-          S.not(S.equals("get")(S.toLower(S.fst(urlPair))))
-        )(S.pipe([fetchURL(initAttrs), S.chain(toJSON)]))(drainURL(initAttrs)),
-      ])(urlPair);
-    };
+  return S.pipe([
+    S.map(toURL),
+    whenGetMethod(S.map(setDefaultSearchParam("per_page", "100"))),
+    S.ifElse((urlPair) =>
+      fetch_one === true ||
+      S.not(S.equals("get")(S.toLower(S.fst(urlPair))))
+    )(S.pipe([fetchURL(initAttrs), S.chain(toJSON)]))(drainURL(initAttrs)),
+  ])(urlPair);
+};
