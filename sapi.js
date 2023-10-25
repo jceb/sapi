cf1af725f1a2e13133ffece989abcb1b65d98a98
Jan Christoph Ebersbach <jceb@e-jc.de>
2023-10-25 14:42:38 +0200

    feat: hint the number of pages that are going to be fetched

--- sapi.js
+++ sapi.js
@@ -6,14 +6,14 @@ F.fork(console.error)(console.log)(
   S.pipe([
     buildURL(Deno.env.get("URL")),
     S.map(S.pipe([
-      gitLabFetch(Boolean(Deno.env.get("P")))(
+      gitLabFetch(Boolean(Number(Deno.env.get("P") || "0")))(
         Deno.env.get("TOKEN"),
       ),
       S.map((res) => JSON.stringify(res)),
     ])),
     S.fromMaybe(
       F.reject(
-        "Insufficient arguments provided\n\tURL, TOKEN and P (number of pages to fetch) must be set\n\tUsage: URL=https://... TOKEN=xyz P=1 sapi HTTPVERB PATH_OF_URL",
+        "Insufficient arguments provided\n\tURL, TOKEN and P (if set, fetch only one page) must be set\n\tUsage: URL=https://... TOKEN=xyz P=1 sapi HTTPVERB PATH_OF_URL",
       ),
     ),
     // log("nix mehr?"),
